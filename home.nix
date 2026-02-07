9fe093d4649f37b4d1f4c6814a6e842b423164c8
NixOS_Dom <dbabic1903@gmail.com>
2026-02-07 22:38:50 +0100

    Configured Fuzzel a bit

--- home.nix
+++ home.nix
@@ -15,15 +15,15 @@
     };
   };
   
-  #Alacritty
+  #Alacritty - Super+T in the default setting (terminal)
   programs.alacritty = {
     enable = true;
     settings = {
       window.opacity = 0.9;
-      font.normal = {
-        family = "JetBrains Mono";
-        style = "Regular";
-      };
+      #font.normal = {
+      #  family = "JetBrains Mono";
+      #  style = "Regular";
+      #};
       font.size = 16;
     };
   };
@@ -39,9 +39,36 @@
     --style="numbers,changes,grid"
     --paging=auto
   '';
+  
+  #Niri Configuration
+  #home.file.".config/niri/config.kdl".text = ''
+  #  binds {
+  #    Mod+Space repeat=false { close-window;}
+  #  }
+  #'';
+  
+  #Fuzzel Configuration
+  programs.fuzzel = {
+    enable = true;
+    settings = {
+      main = {
+        #hide-before-typing = "true";
+        lines = 8;
+      };
+      #colors.background = "00000000";
+    };
+  };
+ 
+  #Niri and Setup
+  programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
+  programs.waybar.enable = true; # launch on startup in the default setting (bar)
+  services.mako.enable = true; # notification daemon
+  services.swayidle.enable = true; # idle management daemon
+  services.polkit-gnome.enable = true; # polkit
 
   #Installing packages
   home.packages = with pkgs; [
     bat
+    swaybg #wallpaper
   ];
 }
